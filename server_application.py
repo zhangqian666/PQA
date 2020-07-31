c7792e60aa3def802a2432977de3f083e80d9f67
zhangqian <415677303@qq.com>
2020-07-31 13:36:25 +0800

    add_flask

---
+++ server_application.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: zhangqian
+
+@contact: 
+
+@Created on: 2020-07-31 07:55
+"""
+
+from flask import Flask, render_template, request
+from RE.relation_extraction import RelationModel
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/question', methods=["POST", "GET"])
+def get_question():
+    question = request.values.get("message")
+    relationModel = RelationModel()
+    result_answer = relationModel.Other_simple_parse(question)
+    result_data = {"data": "查询成功： {}".format(result_answer)}
+    return result_data
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8777)
