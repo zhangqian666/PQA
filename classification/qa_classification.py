36d536ec35b985c5eca079daf2b6006b5de5252d
zhangqian <415677303@qq.com>
2020-08-07 11:42:24 +0800

    整体测试

--- classification/qa_classification.py
+++ classification/qa_classification.py
@@ -11,7 +11,7 @@ from bert_base.client import BertClient
 
 
 def classif(question):
-    bc = BertClient(port=6666, port_out=6667, mode="CLASS")
+    bc = BertClient(port=5559, port_out=5560, mode="CLASS")
     result = bc.encode([question])
 
     return result[0]["pred_label"]
